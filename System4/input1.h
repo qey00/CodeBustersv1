739c58d66959cb44e6c5658b50b116ff7440a7db
qey00 <dguzmanoutputs@gmail.com>
2023-05-19 09:06:19 +0800

    initial commit

--- System4/input1.h
+++ System4/input1.h
@@ -48,11 +48,17 @@ namespace System4 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"input";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->SuspendLayout();
+			// 
+			// input
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(784, 411);
+			this->Name = L"input";
+			this->Text = L"input";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};
